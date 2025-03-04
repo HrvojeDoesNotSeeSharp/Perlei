f9819f86b61172f2a9907e918b482cc31b977b2c
Hrvoje Vukovic <hvukovi@gmail.com>
2025-03-04 19:59:33 +0100

    fix navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -7,33 +7,33 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
+//import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+//import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import { Grid2 } from '@mui/material';
 
 const pages = [{ text: 'Usluge', link: '#usluge' }, { text: 'Galerija', link: '#gallery' }, { text: 'O Nama', link: '#about' }, { text: 'Cijene', link: '#cijene' }, { text: 'Rezervacije', link: '#reservation' },]
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+//const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function NavBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  //const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  /*const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
-  };
+  };*/
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
 
-  const handleCloseUserMenu = () => {
+  /*const handleCloseUserMenu = () => {
     setAnchorElUser(null);
-  };
+  };*/
 
   return (
     <Grid2 id="navbar" position="static" size={12}>
